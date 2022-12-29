947cda490c99f597ec7b53f946134cf5b0a3b026
TpCBB <331499622@qq.com>
2022-12-29 17:15:57 +0800

    toxpy

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      "/weixin": {
+        target: "https://api.weixin.qq.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/weixin/, ""),
+      },
+      "/weather": {
+        target: "http://autodev.openspeech.cn",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/weather/, ""),
+      },
+      "/sweetword": {
+        target: "https://api.1314.cool",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/sweetword/, ""),
+      },
+    },
+  },
+})
